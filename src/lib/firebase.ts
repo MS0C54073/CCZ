e5f3cded955bda26cc4b3f4886abac36c2fd0cea
MU30 <salimusonda1@gmail.com>
2025-07-22 00:23:52 +0000

    auth configuration not found in firebase, fix this error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getMessaging } from "firebase/messaging";
+// import { getMessaging } from "firebase/messaging";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "career-compass-x1f43",
-  "appId": "1:453008602434:web:f41c0dc612a36d83da84db",
-  "storageBucket": "career-compass-x1f43.firebasestorage.app",
-  "apiKey": "AIzaSyBqQCDIr1hQd2dVL7B7mUW7a6Z7qOgKgYc",
-  "authDomain": "career-compass-x1f43.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "453008602434"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
